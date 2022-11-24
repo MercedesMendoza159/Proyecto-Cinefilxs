c25bf3c70b69c1df6c7b7f03c905ce2b927bef8a
Mercedes Mendoza <mercedes.mdz97@gmail.com>
2022-11-24 18:09:47 -0300

    primera clase js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,69 @@
+//declARACION
+let nombre;
+let nombreCompleto;
+let apellido;
+let numero1;
+let numero2;
+let resultado;
 
+
+//asignacion 
+apellido = "Garay";
+numero1 = 10;
+numero2 = 22;
+apellido = "Garay Benitez";
+
+let multi = numero1 * numero2;
+
+//constantes, no cambian los valores
+
+const IVA = 21;
+const PI = 13.1416;
+
+resultado = IVA + 1400;
+
+//concatenacion
+nombre = "juanito";
+apellido = "arcoiris";
+nombreCompleto = nombre + " " + apellido;
+
+
+//console.log para poder ver datos por consola 
+
+/*console.log(resultado);
+console.log(nombreCompleto);
+console.log(multi/2)
+console.log("el valor de PI es:" + PI )*/
+
+//ALERT
+
+//alert("soy un mensaje molesto")
+
+//entrada de datos promp
+
+//let ingresoNombre = prompt("Ingresa tu nombre");
+//console.log(ingresoNombre)
+
+//let saludo = "Hola" + " " + ingresoNombre + " " + "Como estas?"
+//alert(saludo)
+
+/*numero1 = prompt ("Ingresa un numero");
+console.log (numero1);
+
+numero2 = prompt ("Ingresa un numero");
+console.log (numero2);
+
+resultado = numero1 + numero2;
+console.log (resultado);*/
+
+//parseo de numeros 
+
+numero1 = parseFloat (prompt ("Ingresa un numero"));
+console.log (numero1);
+
+numero2 = parseFloat (prompt ("Ingresa un numero"));
+console.log (numero2);
+
+resultado = numero1 + numero2;
+console.log (resultado);
 
